3f35060d1f20269f76e9f314c96dbfccf01aba67
IstiakMasrurSaad <istiakmasrursaad.cse@gmail.com>
2025-08-29 14:35:13 +0600

    some changes in responsive added

--- script.js
+++ script.js
@@ -1,27 +1,23 @@
-
 // heartCounting function
 function heartNumber(id) {
-    const heartValue = document.getElementById(id).innerText;
-    return parseInt(heartValue);
+  const heartValue = document.getElementById(id).innerText;
+  return parseInt(heartValue);
 }
 let initialHearts = document.getElementsByClassName("heart");
 for (let i = 0; i < initialHearts.length; i++) {
-    initialHearts[i].addEventListener("click", function () {
-        let heartValue = heartNumber("heart-count");
-        let heartIncrease = heartValue + 1;
-        document.getElementById("heart-count").innerText = heartIncrease;
-        // alert("Heart added");
-    });
+  initialHearts[i].addEventListener("click", function () {
+    let heartValue = heartNumber("heart-count");
+    let heartIncrease = heartValue + 1;
+    document.getElementById("heart-count").innerText = heartIncrease;
+    alert("Heart added");
+  });
 }
-
 
 //copy counter
 function copyCounter(id) {
-    let text = document.getElementById(id).innerText;
-    navigator.clipboard.writeText(text);
-    alert(text+" -Copied to clipboard & copt counter increased");
-    let copyCount = parseInt(document.getElementById("copy-count").innerText);
-    document.getElementById("copy-count").innerText = copyCount + 1;
+  let text = document.getElementById(id).innerText;
+  navigator.clipboard.writeText(text);
+  alert(text + " -Copied to clipboard & copt counter increased");
+  let copyCount = parseInt(document.getElementById("copy-count").innerText);
+  document.getElementById("copy-count").innerText = copyCount + 1;
 }
-
-
